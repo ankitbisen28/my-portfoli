4b1c822cfc7057ff71ba5c12cbe38da40081e944
Ankit Bisen <ankitbisen288@gmail.com>
2023-12-19 10:48:44 +0530

    added Navbar

---
+++ client/src/theme.js
@@ -0,0 +1,37 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins,sans-serif",
+    h1: {
+      fontSize: "2rem",
+      fontWeight: "600",
+    },
+  },
+  palette: {
+    primary: {
+      main: "#212121",
+    },
+    secondary: {
+      main: "#0bceaf",
+    },
+    info: {
+      main: "#ffc200",
+    },
+    grey: {
+      main: "#474559",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  // ... other theme options ...
+});
+
+export default theme;
