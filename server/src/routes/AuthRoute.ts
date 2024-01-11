3eef2f34ff882b8eebd93a94a190bb6ab078aa84
Ankit Bisen <ankitbisen288@gmail.com>
2024-01-11 13:41:29 +0530

    server removed

--- server/src/routes/AuthRoute.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { register, login } from "../controller/AuthController";
-
-export default (router: express.Router) => {
-  router.post("/auth/register", register);
-  router.post("/auth/login", login);
-};
