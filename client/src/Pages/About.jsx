5f8eb0dfcb58472e1060e81afbc09a821fa9cc7e
Ankit Bisen <ankitbisen288@gmail.com>
2024-02-03 23:12:01 +0530

    meta description added and also about changed

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -18,15 +18,35 @@ export const About = () => {
         >
           About Me
         </Typography>
-        <Typography variant="body1" width={{xs:"100%", md:"60%" }} marginTop={4} color="primary">
-          I am a Full Stack Developer, with a keen focus on Node.js and
-          React.js. Passionate about crafting seamless and innovative web
-          solutions, I bring a fresh perspective to the world of technology. My
-          academic background, coupled with hands-on experience, has equipped me
-          with the skills needed to navigate the dynamic realm of full-stack
-          development. I am dedicated to leveraging my expertise to contribute
-          to impactful and cutting-edge projects in the ever-evolving field of
-          web development
+        <Typography
+          variant="body1"
+          width={{ xs: "100%", md: "60%" }}
+          marginTop={4}
+          color="primary"
+        >
+          Welcome to the portfolio of Ankit Bisen, a self-taught developer with
+          a passion for crafting dynamic and innovative digital solutions.
+          Specializing in React and Node.js, Ankit possesses a versatile skill
+          set that encompasses both SQL and NoSQL databases. With a track record
+          of participation in multiple hackathons, Ankit thrives in fast-paced,
+          collaborative environments, where creativity and problem-solving are
+          paramount.
+          <br />
+          <br />
+          As a freelancer, Ankit's expertise shines in the development of
+          blog-related and ecommerce websites. With a keen understanding of
+          client needs and industry trends, Ankit delivers tailored solutions
+          that elevate online experiences. Drawing from previous experience with
+          WordPress, Ankit adeptly navigates both custom development and
+          WordPress platforms, ensuring versatility and efficiency in every
+          project undertaken.
+          <br />
+          <br />
+          Explore Ankit Bisen's portfolio to witness the fusion of
+          technical prowess, creativity, and client-centric focus that defines
+          his work. Whether it's building custom solutions or leveraging
+          existing platforms, Ankit is committed to delivering excellence in
+          every endeavor.
         </Typography>
         <Technology />
         <WorkExperience />
